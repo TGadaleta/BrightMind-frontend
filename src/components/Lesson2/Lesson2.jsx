bd05cc4166fb1c3350108c0a8d5c960ba121ced6
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-04 10:43:42 -0500

    finished navigation from lesson page

--- src/components/Lesson2/Lesson2.jsx
+++ src/components/Lesson2/Lesson2.jsx
@@ -1,7 +1,7 @@
 import styles from './Lesson2.module.css';
-import { useEffect, useState, useContext } from 'react';
 import { AuthedUserContext } from "../../App";
 import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
 import * as userServices from '../../services/userServices.js';
 
 const Lesson2 = () => {
@@ -42,38 +42,8 @@ const Lesson2 = () => {
 				{lesson.name || 'No lesson name available'}
 			</h1>
 			<p>{lesson.text || 'No content available for this lesson.'}</p>
-
-			{lesson.course && (
-				<>
-					<h2>Course Name: {lesson.course.name}</h2>
-					<h3>Department: {lesson.course.department}</h3>
-					<p>{lesson.course.description}</p>
-
-					{lesson.course.lessons && lesson.course.lessons.length > 0 && (
-						<div className={styles.lessonsContainer}>
-							<h2>Lessons</h2>
-							<div className={styles.lessonList}>
-								{lesson.course.lessons.map((lessonItem) => (
-									<div key={lessonItem._id} className={styles.lessonCard}>
-										<p>
-											<strong>Name:</strong> {lessonItem.name || 'N/A'}
-										</p>
-										<p>
-											<strong>Description:</strong> {lessonItem.text || 'TBA'}
-										</p>
-										<Link to={`/courses/lessons/${lessonItem._id}`}>
-											View Lesson
-										</Link>
-									</div>
-								))}
-							</div>
-						</div>
-					)}
-				</>
-			)}
-
 			<div className={styles.navTabs}>
-				<Link to='../Courses'>
+				<Link to={`/courses/${lesson.course}`}>
 					<button className={styles.backButton}>Back</button>
 				</Link>
 			</div>
